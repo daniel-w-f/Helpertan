41f94ed8ff568ea0d01f40cff247f227be24be64
daniel-w-f <daniel_wilms@gmx.de>
2023-05-29 20:42:05 +0200

    Refactor phyton files into separate folder

--- phyton/gettingstarted.py
+++ phyton/gettingstarted.py
@@ -1,5 +1,8 @@
 # import python discord library
+from ast import List
 import discord
+from discord.ext import commands
+import random
 
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.DEBUG)
@@ -37,6 +40,27 @@ async def on_message(message):
     if message.content.startswith('hello'):
         # send a message to the channel
         await message.channel.send('Hello!')
+
+# set app_commands ping, respond with pong
+@discord.app_commands.command(name='ping', description='Respond with pong', nsfw=False)
+async def ping(ctx):
+    await ctx.send('pong')
+
+app_commands = discord.app_commands
+@app_commands.command()
+async def fruits(interaction: discord.Interaction, fruit: str):
+    await interaction.response.send_message(f'Your favourite fruit seems to be {fruit}')
+
+@fruits.autocomplete('fruit')
+async def fruits_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+) -> List[app_commands.Choice[str]]:
+    fruits = ['Banana', 'Pineapple', 'Apple', 'Watermelon', 'Melon', 'Cherry']
+    return [
+        app_commands.Choice(name=fruit, value=fruit)
+        for fruit in fruits if current.lower() in fruit.lower()
+    ]
 
 # load the token from a file called token.txt
 with open('token.txt', 'r') as f:
